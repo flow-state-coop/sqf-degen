54f8d92094dec8512e931828aa6bb926d8ec1797
Xian Zheng <seeinplay@gmail.com>
2024-05-14 08:36:58 -0600

    update messages

--- frames/app/frames/grantee/[address]/[pool]/route.tsx
+++ frames/app/frames/grantee/[address]/[pool]/route.tsx
@@ -15,14 +15,28 @@ const handler = async (req: NextRequest) => {
 
     return {
       image: (
-        <div tw='flex relative'>
-          <h1>SQF Funding For.. {address}</h1>
-          <h1>Pool.. {pool}</h1>
-        </div>
+        <span tw='flex flex-col px-10'>
+          <h3>Streaming QF- Degen Builders Round</h3>
+          <h3>
+            SQF Funding For: {address} Pool: {pool}
+          </h3>
+          <p>
+            Open a $DEGEN donation stream that's matched with quadratic funding.
+          </p>
+          <p>
+            The more DEGENS that donate, the higher the matching multiplier!
+          </p>
+        </span>
       ),
       buttons: [
-        <Button action='post' target='/grantee'>
-          Previous frame
+        <Button action='link' target={`https://sqf-degen-ui.vercel.app/`}>
+          SQF Round Details
+        </Button>,
+        <Button action='tx' target='/stream/wrapDegen' post_url='/stream/'>
+          Wrap to DegenX
+        </Button>,
+        <Button action='tx' target='/stream/donate' post_url='/stream/success'>
+          Donate
         </Button>,
       ],
     };
