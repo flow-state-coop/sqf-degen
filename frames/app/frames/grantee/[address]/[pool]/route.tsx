ea0093baab2cf5e931fedc5e8eb60938fd164394
Xian Zheng <seeinplay@gmail.com>
2024-05-14 08:07:41 -0600

    get dynamic grantee with pool

--- frames/app/frames/grantee/[address]/[pool]/route.tsx
+++ frames/app/frames/grantee/[address]/[pool]/route.tsx
@@ -1,5 +1,5 @@
 import { Button } from "frames.js/next";
-import { frames } from "../../frames";
+import { frames } from "../../../frames";
 import { NextRequest } from "next/server";
 
 const handler = async (req: NextRequest) => {
@@ -8,26 +8,26 @@ const handler = async (req: NextRequest) => {
   const pathSegments = url.pathname.split("/");
 
   const address = pathSegments[3];
+  const pool = pathSegments[4];
 
   return await frames(async (ctx) => {
-    const path = `frames/grantee/${address}/pool/1`;
+    const path = `/grantee/${address}`;
 
     return {
       image: (
         <div tw='flex relative'>
           <h1>SQF Funding For.. {address}</h1>
+          <h1>Pool.. {pool}</h1>
         </div>
       ),
       buttons: [
         <Button action='post' target='/grantee'>
           Previous frame
         </Button>,
-        <Button action='post' target={path}>
-          View Details
-        </Button>,
       ],
     };
   })(req);
 };
+
 export const POST = handler;
 export const GET = handler;
