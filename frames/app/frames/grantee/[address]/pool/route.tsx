d1adb8d0948a409fa8d8f304323ee5ecf0f60e3f
Xian Zheng <seeinplay@gmail.com>
2024-05-13 19:02:05 -0600

    fix delpoyment errors

--- frames/app/frames/grantee/[address]/pool/route.tsx
+++ frames/app/frames/grantee/[address]/pool/route.tsx
@@ -1,15 +1,11 @@
 // pages/frames/grantee/[grantee]/pool/[pool].js
-import { useRouter } from "next/router";
 
 const PoolPage = () => {
-  const router = useRouter();
-  const { grantee, pool } = router.query;
-
   return (
     <div>
       <h1>Pool Details</h1>
-      <p>Grantee ID: {grantee}</p>
-      <p>Pool ID: {pool}</p>
+      <p>Grantee ID: </p>
+      <p>Pool ID:</p>
     </div>
   );
 };
