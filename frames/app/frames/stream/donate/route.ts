9ca14c4dc68c7a57e48ae5955bca6d20181ee49f
Xian Zheng <seeinplay@gmail.com>
2024-05-18 11:36:13 -0600

    get address and pool from query params

--- frames/app/frames/stream/donate/route.ts
+++ frames/app/frames/stream/donate/route.ts
@@ -7,46 +7,44 @@ import {
   http,
   parseEther,
 } from "viem";
-import { degen } from "viem/chains";
 import { cfaForwarderAbi } from "../../../lib/abi/cfaForwarder";
-import { frames } from "../../frames";
 
 const handler = async (req: NextRequest, res: NextResponse) => {
-  return await frames(async (ctx) => {
-    const json = await req.json();
+  const json = await req.json();
 
-    const frameMessage = await getFrameMessage(json);
-    const address = ctx.state.address;
-    const pool = ctx.state.pool;
-    const SECONDS_IN_MONTH = BigInt(2628000);
+  const frameMessage = await getFrameMessage(json);
+  const { searchParams } = new URL(req.url);
 
-    const cfaForwarderAddress = "0xcfA132E353cB4E398080B9700609bb008eceB125";
-    const degenxAddress = "0xda58FA9bfc3D3960df33ddD8D4d762Cf8Fa6F7ad";
-    const monthlyFlowRate = parseEther("1"); // 1 DEGENx per month
-    const flowRate = monthlyFlowRate / SECONDS_IN_MONTH; // 380517503805 wei per second
+  const address = searchParams.get("address");
+  const pool = searchParams.get("pool");
+  const SECONDS_IN_MONTH = BigInt(2628000);
 
-    const createFlowCalldata = encodeFunctionData({
-      abi: cfaForwarderAbi,
-      functionName: "createFlow",
-      args: [
-        degenxAddress,
-        frameMessage.connectedAddress,
-        address,
-        flowRate,
-        "0x",
-      ],
-    });
+  const cfaForwarderAddress = "0xcfA132E353cB4E398080B9700609bb008eceB125";
+  const degenxAddress = "0xda58FA9bfc3D3960df33ddD8D4d762Cf8Fa6F7ad";
+  const monthlyFlowRate = parseEther("1"); // 1 DEGENx per month
+  const flowRate = monthlyFlowRate / SECONDS_IN_MONTH; // 380517503805 wei per second
 
-    return NextResponse.json({
-      chainId: "eip155:666666666",
-      method: "eth_sendTransaction",
-      params: {
-        abi: cfaForwarderAbi as Abi,
-        to: cfaForwarderAddress,
-        data: createFlowCalldata,
-        value: "0",
-      },
-    });
+  const createFlowCalldata = encodeFunctionData({
+    abi: cfaForwarderAbi,
+    functionName: "createFlow",
+    args: [
+      degenxAddress,
+      frameMessage.connectedAddress,
+      address,
+      flowRate,
+      "0x",
+    ],
+  });
+
+  return NextResponse.json({
+    chainId: "eip155:666666666",
+    method: "eth_sendTransaction",
+    params: {
+      abi: cfaForwarderAbi as Abi,
+      to: cfaForwarderAddress,
+      data: createFlowCalldata,
+      value: "0",
+    },
   });
 };
 
